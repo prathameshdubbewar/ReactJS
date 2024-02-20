e198ff026687daae2a95fbeb4ca8a2022b538c20
Prathamesh <prathameshdubbewar@gmail.com>
2024-02-20 23:51:55 +0530

    Done

---
+++ components/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <h1 classname = 'hi'> hiiii i am <span>Pratham</span></h1>
+    </div>
+  );
+}
+
+export default App;
